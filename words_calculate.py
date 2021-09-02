47147cfce3b2b3288f0b4a7d50b43b2bef1e2c5c
laughing-coder <laughinl_coder@protonmail.com>
2021-09-02 12:14:29 +0200

    rename variables, reformat code to PEP8

--- words_calculate.py
+++ words_calculate.py
@@ -1,9 +1,9 @@
-mystery_string = str(input("please enter your string "))
+my_string = str(input("please enter your string "))
 
-word = str(input("input your word "))
+word = str(input("please enter your word "))
 count = 0
-for i in range(len(mystery_string) - len(word) + 1):
-    if mystery_string[i:i + len(word)] == word:
+
+for i in range(len(my_string) - len(word) + 1):
+    if my_string[i:i + len(word)] == word:
         count += 1
 print(count)
-
