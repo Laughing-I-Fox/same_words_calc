6cad7103bb99f8e7ba1ba7208aa5bba28b5de3b2
laughing-coder <laughinl_coder@protonmail.com>
2021-09-02 12:51:28 +0200

    rename project

--- words_calculate.py
+++
@@ -1,9 +0,0 @@
-my_string = str(input("please enter your string "))
-
-word = str(input("please enter your word "))
-count = 0
-
-for i in range(len(my_string) - len(word) + 1):
-    if my_string[i:i + len(word)] == word:
-        count += 1
-print(count)
